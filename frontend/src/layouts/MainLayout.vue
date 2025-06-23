e797413f909718ce869a9de43f01c235e8993824
Adriano Heissig <adriano@rosset.com.br>
2025-06-23 11:20:52 -0300

    Acerto no menu

--- frontend/src/layouts/MainLayout.vue
+++ frontend/src/layouts/MainLayout.vue
@@ -2,8 +2,8 @@
   <q-layout view="lHh Lpr lFf">
     <template v-if="!isLoginPage">
       <q-header elevated>
-          <q-toolbar>
-            <q-btn
+        <q-toolbar>
+          <q-btn
             flat
             dense
             round
@@ -12,7 +12,7 @@
             @click="toggleLeftDrawer"
           />
           <q-toolbar-title>
-          <q-img src="/logo.png" width="40px" />
+            <q-img src="/logo.png" width="40px" />
             Haras System
           </q-toolbar-title>
           <q-btn 
@@ -46,43 +46,56 @@
               <q-item-label>Perfil</q-item-label>
             </q-item-section>
           </q-item>
-          <q-item clickable v-ripple to="/terrenos">
-            <q-item-section avatar>
-              <q-icon name="landscape" />
-            </q-item-section>
-            <q-item-section>Terrenos</q-item-section>
-          </q-item>
-          <q-item clickable v-ripple to="/animais">
-            <q-item-section avatar>
-              <q-icon name="pets" />
-            </q-item-section>
-            <q-item-section>Animais</q-item-section>
-          </q-item>
-          <q-item clickable v-ripple to="/crescimento-saude">
-            <q-item-section avatar>
-              <q-icon name="trending_up" />
-            </q-item-section>
-            <q-item-section>Crescimento & Saúde</q-item-section>
-          </q-item>
-          <q-item clickable v-ripple to="/movimentacoes">
-            <q-item-section avatar>
-              <q-icon name="swap_horiz" />
-            </q-item-section>
-            <q-item-section>Movimentações</q-item-section>
-          </q-item>          
-          <q-item clickable v-ripple to="/reproducao">
-            <q-item-section avatar>
-              <q-icon name="favorite" />
-            </q-item-section>
-            <q-item-section>Reprodução</q-item-section>
-          </q-item>          
-          <q-item clickable v-ripple to="/manejo">
-            <q-item-section avatar>
-              <q-icon name="eco" />
-            </q-item-section>
-            <q-item-section>Manejo de Terrenos</q-item-section>
-          </q-item>          
-          <!-- Adicionar mais itens conforme necessário -->
+          <q-expansion-item
+            expand-separator
+            icon="pets"
+            label="Animais"
+            default-opened
+          >
+            <q-item to="/animais" clickable v-ripple>
+              <q-item-section avatar>
+                <q-icon name="create" />
+              </q-item-section>
+              <q-item-section>Cadastro de Animais</q-item-section>
+            </q-item>
+            <q-item to="/crescimento-saude" clickable v-ripple>
+              <q-item-section avatar>
+                <q-icon name="trending_up" />
+              </q-item-section>
+              <q-item-section>Crescimento & Saúde</q-item-section>
+            </q-item>
+            <q-item to="/reproducao" clickable v-ripple>
+              <q-item-section avatar>
+                <q-icon name="favorite" />
+              </q-item-section>
+              <q-item-section>Reprodução</q-item-section>
+            </q-item>
+            <q-item to="/movimentacoes" clickable v-ripple>
+              <q-item-section avatar>
+                <q-icon name="swap_horiz" />
+              </q-item-section>
+              <q-item-section>Movimentações</q-item-section>
+            </q-item>
+          </q-expansion-item>
+          <q-expansion-item
+            expand-separator
+            icon="landscape"
+            label="Terrenos"
+            default-opened
+          >
+            <q-item to="/terrenos" clickable v-ripple>
+              <q-item-section avatar>
+                <q-icon name="create" />
+              </q-item-section>
+              <q-item-section>Cadastro de Terrenos</q-item-section>
+            </q-item>
+            <q-item to="/manejo" clickable v-ripple>
+              <q-item-section avatar>
+                <q-icon name="eco" />
+              </q-item-section>
+              <q-item-section>Manejo de Terrenos</q-item-section>
+            </q-item>
+          </q-expansion-item>
         </q-list>
       </q-drawer>    
     </template>
@@ -93,11 +106,10 @@
 </template>
 
 <script>
-
 import { useAuthStore } from 'src/stores/auth';
 import { useQuasar } from 'quasar';
 import { useRouter, useRoute } from 'vue-router';
-import { computed,ref } from 'vue';
+import { computed, ref } from 'vue';
 
 export default {
   name: 'MainLayout',
@@ -107,7 +119,7 @@ export default {
     const router = useRouter();
     const route = useRoute();
     const leftDrawerOpen = ref(false);
-    const standAlonePages = ['/login','/forgot-password', '/reset-password']
+    const standAlonePages = ['/login', '/forgot-password', '/reset-password'];
     
     const isLoginPage = computed(() => standAlonePages.includes(route.path));
 
